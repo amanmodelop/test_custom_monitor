bc0834c7133e8eb87639667e3a8938b0a30b6280
amanmodelop <156818279+amanmodelop@users.noreply.github.com>
2024-10-05 01:07:05 -0400

    Update test_monitor_output.py

--- test_monitor_output.py
+++ test_monitor_output.py
@@ -38,8 +38,8 @@ def init(init_param):
 def metrics(data: pd.DataFrame):
     print("Running the metrics function") 
     config=load_config("./tables.json")
-    print(data["Risk Factors"])
-    print(data.loc[:,'Risk Factors'].loc['field1'])
+    print(list(data["Risk Factors"].items())
+    print(data['Risk Factors'].loc['field1'])
     field1=data.get("Risk Factors").get("field1")	
     field2=data.get("Risk Factors").get("field2")
     field3=data.get("Risk Factors").get("field3")
