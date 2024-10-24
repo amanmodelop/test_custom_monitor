828632653d360533253afdd3d5827489d54c1852
amanmodelop <156818279+amanmodelop@users.noreply.github.com>
2024-10-24 09:50:10 -0400

    Update test_monitor_output.py

--- test_monitor_output.py
+++ test_monitor_output.py
@@ -38,18 +38,18 @@ def init(init_param):
 def metrics(data: pd.DataFrame):
     print("Running the metrics function") 
     config=load_config("./tables.json")
-    print(data['Risk Management'][0].get('What is the algorithmic complexity of the AI Product?'))
-    field1=data['Risk Management'][0].get('What is the final state of the training data?')
-    field2=data['Risk Management'][0].get('Select the highest data classification used to build/use the AIP (e.g., training, validation, input data).')
-    field3=data['Risk Management'][0].get('What is the acceptable predictive accuracy of the AI Product relative to expected performance target(s)?')
-    field4=data['Risk Management'][0].get('Is the data used in the AI Product (e.g., training, validation, input, grounding) subject to data use restrictions?')
-    field5=data['Risk Management'][0].get('How controlled is the input data field?')
+    field1=data['Risk Management'][0].get('What is the algorithmic complexity of the AI Product?').split()[0]
+    field2=data['Risk Management'][0].get('What is the final state of the training data?').split()[0]
+    field3=data['Risk Management'][0].get('Select the highest data classification used to build/use the AIP (e.g., training, validation, input data).').split()[0]
+    field4=data['Risk Management'][0].get('What is the acceptable predictive accuracy of the AI Product relative to expected performance target(s)?').split()[0]
+    field5=data['Risk Management'][0].get('Is the data used in the AI Product (e.g., training, validation, input, grounding) subject to data use restrictions?').split()[0]
+    field6=data['Risk Management'][0].get('How controlled is the input data field?').split()[0]
 
     #other questions
-    field6=data['Overview'][0].get('Select the intended use of the AI Product.')
-    field7=data['Overview'][0].get('What is the opportunity cost of not having the AI Product?')
-    field8=data['Overview'][0].get('What is the impact to business operations if the AIP is not available for use?')
-    field9=data['Risk Management'][0].get("What is the potential impact to a data subject's (i.e., Employees, Customers, Consumers, Prospects, Applicants, etc.) legal rights (or similar) from an AIP decision?")
+    field7=data['Overview'][0].get('Select the intended use of the AI Product.')[:1]
+    field8=data['Overview'][0].get('What is the opportunity cost of not having the AI Product?').split()[0]
+    field9=data['Overview'][0].get('What is the impact to business operations if the AIP is not available for use?').split()[0]
+    field10=data['Risk Management'][0].get("What is the potential impact to a data subject's (i.e., Employees, Customers, Consumers, Prospects, Applicants, etc.) legal rights (or similar) from an AIP decision?").split()[0]
 
 
     
